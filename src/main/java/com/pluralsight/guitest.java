8770284737591d843a6f3861b9516e55fd6773e4
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-10-14 22:38:14 -0400

    Restructured JavaFX Testing Files

--- src/main/java/com/pluralsight/guitest.java
+++ src/main/java/com/pluralsight/guitest.java
@@ -1,9 +1,14 @@
 package com.pluralsight;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+import java.awt.*;
 
 public class guitest extends Application{
 
@@ -13,8 +18,14 @@ public class guitest extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
+        Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
         Scene scene = new Scene(root);
+
+        Image icon = new Image("icon.png");
+        stage.getIcons().add(icon);
+        stage.setTitle("Ledger App");
+//        stage.setHeight(800);
+//        stage.setWidth(800);
 
         stage.setScene(scene);
         stage.show();
